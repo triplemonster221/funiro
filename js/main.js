38795bd7adbe6b294d7948b8556e475edc2b2c70
triplemonster221 <Boorder1993@yandex.ru>
2021-07-06 23:13:56 +0500

    added ferst-banner

---
+++ js/main.js
@@ -0,0 +1,13 @@
+ $(document).ready(function(){
+
+
+      $('.banner').slick({
+        arrows: true,
+        dots: true,
+        prevArrow: '<button type="button" class="left slick-prev"><img src="images/Left.svg"></button>',
+        nextArrow: '<button type="button" class="right slick-next"><img src="images/Right.svg"></button>',
+      });
+
+
+
+    });
