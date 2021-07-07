45ff84600a506f9ff6811fd18779cf7865b280d7
triplemonster221 <boorder1993@yandex.ru>
2021-07-07 09:48:35 +0300

    added tips slaider

--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@
         nextArrow: '<button type="button" class="right slick-next"><img src="images/Right.svg"></button>',
       });
 
+      $('.tipsslaider__item').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        prevArrow: '<button type="button" class="tipsslaide-left slick-prev"><img src="images/Right.svg"></button>',
+        nextArrow: '<button type="button" class="tipsslaide-right slick-next"><img src="images/Right.svg"></button>',
+      });
+
       $('.product__list-like').on('click',function(e){
         e.preventDefault();
         $(this).toggleClass('product__list-like--active')
